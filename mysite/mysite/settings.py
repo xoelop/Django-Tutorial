83fe5195cda7e3b487f48a3e2149a9a3b4746614
Xoel López Barata <xoelop@gmail.com>
2018-05-01 12:34:15 +0800

    part 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['0.0.0.0']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
